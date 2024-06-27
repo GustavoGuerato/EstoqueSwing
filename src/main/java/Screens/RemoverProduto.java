88f195fbe961d5b26ce7d1f788493531ec9e3956
GustavoGuerato <guerato.gustavo14@gmail.com>
2024-06-27 13:27:06 -0300

    criacao da tela de remover produtos

---
+++ src/main/java/Screens/RemoverProduto.java
@@ -0,0 +1,15 @@
+package Screens;
+
+import javax.swing.*;
+
+public class RemoverProduto extends JFrame {
+    private JTextField nome;
+    private JTextField codigo;
+    private JTextField validade;
+    private JTextField quantidade;
+    private JTextField preco;
+    private JButton delProduto;
+    private JButton voltarHome;
+
+    
+}
