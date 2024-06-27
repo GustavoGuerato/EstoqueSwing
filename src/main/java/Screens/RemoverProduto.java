46d8813d21a53fd1be0179cd60c6a5e303611e77
GustavoGuerato <guerato.gustavo14@gmail.com>
2024-06-27 16:03:59 -0300

    voltarhome com actionlistener funcionando

--- src/main/java/Screens/RemoverProduto.java
+++ src/main/java/Screens/RemoverProduto.java
@@ -1,15 +1,46 @@
 package Screens;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class RemoverProduto extends JFrame {
-    private JTextField nome;
+public class RemoverProduto extends JFrame{
     private JTextField codigo;
-    private JTextField validade;
-    private JTextField quantidade;
-    private JTextField preco;
-    private JButton delProduto;
+    private JButton verificarButton;
+    private JButton confirmarButton;
     private JButton voltarHome;
 
+    public RemoverProduto(){
+
     
+
+
+        voltarHome.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                Object opcoes[] = {"sim","não"};
+                if (codigo.getText().isEmpty()){
+                    JOptionPane.showMessageDialog(null,"Fechando o programa");
+                    RemoverProduto.this.dispose();
+                }else {
+                    int escolha = JOptionPane.showOptionDialog(null,
+                            "Você quer apagar todos os dados e voltar para o menu principal?",
+                            "Apagar?",
+                            JOptionPane.YES_NO_OPTION,
+                            JOptionPane.WARNING_MESSAGE,
+                            null,
+                            opcoes,
+                            opcoes[1]);
+                    if (escolha == JOptionPane.YES_OPTION){
+                        JOptionPane.showMessageDialog(null, "Apagando o dados e fechando");
+                        RemoverProduto.this.dispose();
+                        new HomeScreen().setVisible(true);
+                    }else {
+                        JOptionPane.showMessageDialog(null, "Ação Cancelada");
+                    }
+                }
+            }
+        });
+    }
 }
